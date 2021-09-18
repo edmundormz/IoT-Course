2e5fc61b8d864c889926a6df27e362041a4cac0b
H. Edmundo Ramírez <edmundormz@gmail.com>
2021-09-18 12:28:33 -0500

    Update S0_LED_Blink_Millis.ino

--- S0_LED_Blink_Millis/S0_LED_Blink_Millis.ino
+++ S0_LED_Blink_Millis/S0_LED_Blink_Millis.ino
@@ -1,12 +1,8 @@
-const int ledPin =  13;      // the number of the LED pin
-int ledState = LOW;          // ledState used to set the LED
-
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
-
-// constants won't change :
-const long interval = 500;           // interval at which to blink (milliseconds)
+// Global variables
+const int ledPin =  13;               // the number of the LED pin
+int ledState = LOW;                   // ledState used to set the LED
+unsigned long previousMillis = 0;     // will store last time LED was updated
+const long interval = 500;            // interval at which to blink (milliseconds)
 
 void setup() {
   // set the digital pin as output:
@@ -21,18 +17,15 @@ void loop() {
   // the LED is bigger than the interval at which you want to
   // blink the LED.
   unsigned long currentMillis = millis();
-
   if (currentMillis - previousMillis >= interval) {
     // save the last time you blinked the LED
     previousMillis = currentMillis;
-
     // if the LED is off turn it on and vice-versa:
     if (ledState == LOW) {
       ledState = HIGH;
     } else {
       ledState = LOW;
     }
-
     // set the LED with the ledState of the variable:
     digitalWrite(ledPin, ledState);
   }
