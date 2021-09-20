8ea5d47effcd6663e2254e34d885d8fab93fa5d6
H. Edmundo Ramírez <edmundormz@gmail.com>
2021-09-20 15:42:45 -0500

    Updated resources for S5

---
+++ S5_Distance_Humidity_Sensors/S5_Distance_Humidity_Sensors.ino
@@ -0,0 +1,41 @@
+#include <NewPing.h>
+#include <DHT.h>
+ 
+#define TRIGGER_PIN 22
+#define ECHO_PIN 23
+#define MAX_DISTANCE 200
+
+#define DHTPIN 14
+#define DHTTYPE DHT11
+
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+   Serial.begin(115200);
+   dht.begin();
+}
+ 
+void loop() {
+   delay(1000);
+   
+   Serial.print("Distance: ");
+   Serial.print(sonar.ping_cm());
+   Serial.println("cm");
+
+   float h = dht.readHumidity();
+   float t = dht.readTemperature();
+   if (isnan(h) || isnan(t)){
+    Serial.println("Failed to read from DHT sensor!");
+   }
+   float hic = dht.computeHeatIndex(t,h,false);
+   Serial.print("Humidity: ");
+   Serial.print(h);
+   Serial.print("% ,Temperature: ");
+   Serial.print(t);
+   Serial.print("°C ");
+   Serial.print("Heat index: ");
+   Serial.print(hic);
+   Serial.println("°C");
+}
